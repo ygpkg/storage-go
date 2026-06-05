f855f0f47a57ec5ddc2130d466ccee115afcd360
morehao <morehao@qq.com>
2026-06-05 16:03:24 +0800

    feat(types): add StoragePath interface and AccessScheme constants

---
+++ types/path.go
@@ -0,0 +1,13 @@
+package types
+
+type AccessScheme string
+
+const (
+	SchemeS3   AccessScheme = "s3"
+	SchemeFile AccessScheme = "file"
+)
+
+type StoragePath interface {
+	Path() string
+	URL() string
+}
---
+++ types/path_test.go
@@ -0,0 +1,30 @@
+package types
+
+import "testing"
+
+type fakePath struct {
+	pathStr string
+	urlStr  string
+}
+
+func (p *fakePath) Path() string { return p.pathStr }
+func (p *fakePath) URL() string  { return p.urlStr }
+
+func TestStoragePathInterface(t *testing.T) {
+	var p StoragePath = &fakePath{pathStr: "s3://b/k", urlStr: "https://cdn/b/k"}
+	if p.Path() != "s3://b/k" {
+		t.Errorf("Path() = %q, want s3://b/k", p.Path())
+	}
+	if p.URL() != "https://cdn/b/k" {
+		t.Errorf("URL() = %q, want https://cdn/b/k", p.URL())
+	}
+}
+
+func TestAccessSchemeConstants(t *testing.T) {
+	if SchemeS3 != "s3" {
+		t.Errorf("SchemeS3 = %q, want s3", SchemeS3)
+	}
+	if SchemeFile != "file" {
+		t.Errorf("SchemeFile = %q, want file", SchemeFile)
+	}
+}
